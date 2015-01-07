9527916a4e21c612d10ffcd6132490171ed0eaac
Ricardo Harris <ricardoharris@ymail.com>
2015-01-07 09:12:17 -0800

    1st commit, on the 2nd milestone

---
+++ public_html/script.js
@@ -0,0 +1,20 @@
+$("document").ready(function(){
+    
+    $("#bio").css("color", "black");
+    
+    $("div").css("border", "3px solid black");
+    
+    $(".uppercase").css("text-transform", "uppercase");
+    
+    $(".uppercase").css("text-size", "38px");
+    
+    $("body").css("text-align", "center");
+    
+    $("body").css("background-color", "white");
+    
+    $('p:first').css("background-color", "whitesmoke");
+
+    $("div :nth-child(3)").css("color", "yellow");
+    
+});
+
