eca28f19239c12184be191537bb0ec2612169149
Ricardo Harris <ricardoharris@ymail.com>
2015-01-07 20:09:21 -0800

    2nd commit, more style

--- public_html/script.js
+++ public_html/script.js
@@ -1,20 +1,26 @@
 $("document").ready(function(){
     
-    $("#bio").css("color", "black");
+    $("#bio").css("color", "white");
     
-    $("div").css("border", "3px solid black");
+    $("div").css("border", "3px solid blue");
     
     $(".uppercase").css("text-transform", "uppercase");
     
     $(".uppercase").css("text-size", "38px");
     
+    $(".uppercase").css("color", "whitesmoke");
+    
     $("body").css("text-align", "center");
     
     $("body").css("background-color", "white");
     
-    $('p:first').css("background-color", "whitesmoke");
+    $('p:first').css("color", "grey");
 
     $("div :nth-child(3)").css("color", "yellow");
+    
+    $("body").css("background-image", "url(http://loopable.files.wordpress.com/2007/09/bitdance.gif)");
+    
+    $("body").css("background-size", "auto");
     
 });
 
