76b20eda1a091a3b352ba4be2a0e7ffd81f19e65
Ricardo Harris <ricardoharris@ymail.com>
2015-01-13 09:17:49 -0800

    5th commit, working on logo animations

--- public_html/script.js
+++ public_html/script.js
@@ -1,6 +1,6 @@
 $("document").ready(function(){
     
-    $("#bio").css("color", "white");
+    $("#bio").css("color", "black");
     
     $("div").css("border", "3px solid blue");
     
@@ -10,11 +10,13 @@ $("document").ready(function(){
     
     $(".uppercase").css("text-size", "38px");
     
-    $(".uppercase").css("color", "whitesmoke");
+    $(".uppercase").css("border", "3px solid black");
+    
+    $(".uppercase").css("color", "black");
     
     $(".bleh").css("color", "grey");
     
-    $("#jq").css("color", "whitesmoke");
+    $("#jq").css("color", "black");
     
     $("body").css("text-align", "center");
     
@@ -22,13 +24,21 @@ $("document").ready(function(){
 
     $("div :nth-child(3)").css("color", "clear");
     
-    $("body").css("background-image", "url(http://loopable.files.wordpress.com/2007/09/bitdance.gif)");
+    $("body").css("background-image", "url(http://th05.deviantart.net/fs70/PRE/f/2011/114/c/2/portal___personality_cores_by_kirbychu-d3es43q.png)");
     
-    $("body").css("background-size", "contain");
+    $("body").css("background-size", "cover");
     
     $("#jq").bind('click', alertButtonClick);
     
     $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+    
+    $("#replaceWHtml").bind('click', replaceWHtml);
+    $("#replaceWText").bind('click', replaceWText);
+    $("#addAPara").bind('click', addAPara);
+    $("#removeAPara").bind('click', removeAPara);
+    
+    $('#fadeLogo').bind('click', fadeTheImage);
+    $('#fullOpacity').bind('click', fullOpacityImage);
 
 });
 
@@ -44,4 +54,28 @@ function mouseOverMe(){
 function mouseOutMe(){
     $("h1").html("JQuery Ex Page");
     $("p").html("");
+}
+
+function replaceWHtml(){
+    $('#h3Tag').html('<h6>Now I am an h6</h6>');
+}
+
+function replaceWText(){
+    $('#h3Tag').text('Now I am just text');
+}
+
+function addAPara(){
+    $('#randPara').append('<p>Another Paragraph</p>');
+}
+
+function removeAPara(){
+    $('#randPara p:last').remove('<h6>Now I am an h6</h6>');
+}
+
+function fadeTheImage(){
+    $('#logo').fadeOut(2500);
+}
+
+function fullOpacityImage(){
+    $('#logo').fadeTo(2500, 1);
 }
